e80f9b7580c9c7dc26d5d6e7c182c080027fbe0d
VictorRandom <victormbalduino@hotmail.com>
2022-02-20 13:49:33 -0300

    importando o componente para o app

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,12 +2,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'courses',
-    template: '<h2>Courses</h2>'
+    //selector vc usa como o nome que vc usara para importar o componente
+    selector: 'courses', 
+    //template é o que irá aparecer na tela quando vc importar o componente
+    template: '<h2>Courses</h2>' 
 })
 
 export class CoursesComponent {
 
-        
+
 
 }
