ddc8ba21ecd25e2d5dca500e2e0b3e9e8309c6fb
VictorRandom <victormbalduino@hotmail.com>
2022-02-20 14:02:47 -0300

    manipualndo os componentes

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,11 +5,15 @@ import { Component } from '@angular/core';
     //selector vc usa como o nome que vc usara para importar o componente
     selector: 'courses', 
     //template é o que irá aparecer na tela quando vc importar o componente
-    template: '<h2>Courses</h2>' 
+    template: '<h2>Title: {{ getTitle() }}</h2>' 
 })
 
 export class CoursesComponent {
 
+    title = 'List of courses';
 
+    getTitle(){
+        return this.title;
+    }
 
 }
